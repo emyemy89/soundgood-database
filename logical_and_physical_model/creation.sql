c034c6e45e6ae50a55d37ca0eec449cf9038a545
emyemy89 <40666117+emyemy89@users.noreply.github.com>
2024-11-17 16:36:12 +0100

    Update creation.sql

--- logical_and_physical_model/creation.sql
+++ logical_and_physical_model/creation.sql
@@ -1,121 +1,122 @@
-
---create tables
 CREATE TABLE person(
-    id serial primary key,
-    first_name varchar(500) not null,
-    last_name varchar(500) not null,
-    personal_num varchar(50) unique not null,
-    email varchar(500),
-    city varchar(500),
-    street varchar(500),
-    zip varchar(50)
+        id SERIAL PRIMARY KEY,
+        first_name   VARCHAR(500) NOT NULL,
+        last_name    VARCHAR(500) NOT NULL,
+        personal_num VARCHAR(50) UNIQUE NOT NULL,
+        email        VARCHAR(500),
+        city         VARCHAR(500),
+        street       VARCHAR(500),
+        zip          VARCHAR(50)
 );
-    
+
 CREATE TABLE student(
-    id serial primary key,
-    level_skills varchar(50) not null,
-    contact_person_id int,               --not null???
-    person_id int not null,
-    foreign key (contact_person_id) references contact_person(contact_person_id),
-    foreign key (person_id) references person(id) --delete cascade??
+        id SERIAL PRIMARY KEY,
+        level_skills      VARCHAR(50) NOT NULL,
+        contact_person_id INT, --not null???
+        person_id         INT NOT NULL,
+        FOREIGN KEY (contact_person_id) REFERENCES contact_person(contact_person_id),
+        FOREIGN KEY (person_id) REFERENCES person(id) --delete cascade??
 );
+
 
 CREATE TABLE contact_person(
-    contact_person_id serial primary key,
-    first_name varchar(500),
-    last_name varchar(500),
-    personal_num varchar(50)
+        contact_person_id SERIAL PRIMARY KEY,
+        first_name   VARCHAR(500),
+        last_name    VARCHAR(500),
+        personal_num VARCHAR(50)
 );
 
 CREATE TABLE contact_phone(
-    phone_nb varchar(50) not null,
-    contact_person_id int not null,
-    foreign key (contact_person_id) references contact_person(contact_person_id) on delete cascade,
-    primary key(phone_nb, contact_person_id)
+        phone_nb          VARCHAR(50) NOT NULL,
+        contact_person_id INT NOT NULL,
+        FOREIGN KEY (contact_person_id) REFERENCES contact_person(contact_person_id) ON DELETE CASCADE,
+        PRIMARY KEY(phone_nb, contact_person_id)
 );
 
 CREATE TABLE instructor(
-    id serial primary key,
-    employment id varchar(500) not null,
-    person_id int not null,
-    foreign key(person_id) references person(id) --delete cascade?
+        id SERIAL PRIMARY KEY,
+        employment ID varchar(500) NOT NULL,
+        person_id     INT NOT NULL,
+        FOREIGN KEY(person_id) REFERENCES person(id) --delete cascade?
 );
 
 CREATE TABLE phone(
-    phone varchar(50) not null,
-    person_id int not null,
-    foreign key(person_id) references person(id)  --delete cascade?
+        phone     VARCHAR(50) NOT NULL,
+        person_id INT NOT NULL,
+        FOREIGN KEY(person_id) REFERENCES person(id) --delete cascade?
 );
 
 CREATE TABLE sibling(
-    sibling_id varchar(50) not null,
-    student_id int not null,
-    foreign key (student_id) references student(id),
-    primary key(sibling_id,student_id)
+        sibling_id VARCHAR(50) NOT NULL,
+        student_id INT NOT NULL,
+        FOREIGN KEY (student_id) REFERENCES student(id),
+        PRIMARY KEY(sibling_id,student_id)
 );
 
 CREATE TABLE student_lesson(
-    student_id int not null,
-    lesson_id int not null,
-    foreign key(student_id) references student(id),
-    foreign key(lesson_id) references lesson(id),
-    primary key(student_id, lesson_id)
+            student_id INT NOT NULL,
+            lesson_id  INT NOT NULL,
+            FOREIGN KEY(student_id) REFERENCES student(id),
+            FOREIGN KEY(lesson_id) REFERENCES lesson(id),
+            PRIMARY KEY(student_id, lesson_id)
 );
 
 CREATE TABLE instructor_availability(
-    availability_date date not null,
-    start_time time not null,
-    end_time time not null,
-    instructor_id int not null,
-    foreign key (instructor_id) references instructor(id),
-    primary key (availability_date, start_time, end_time, instructor_id)
+            availability_date DATE NOT NULL,
+            start_time        TIME NOT NULL,
+            end_time          TIME NOT NULL,
+            instructor_id     INT NOT NULL,
+            FOREIGN KEY (instructor_id) REFERENCES instructor(id),
+            PRIMARY KEY (availability_date, start_time, end_time, instructor_id)
 );
 
-CREATE TABLE rental (
-    id serial primary key,
-    start_date date not null,
-    end_date date not null,
-    student_id int not null,
-    foreign key(student_id) references student(id)
+
+CREATE TABLE rental(
+            id SERIAL PRIMARY KEY,
+            start_date DATE NOT NULL,
+            end_date   DATE NOT NULL,
+            student_id INT NOT NULL,
+            FOREIGN KEY(student_id) REFERENCES student(id)
 );
 
 
 CREATE TABLE instrument(
-    id serial primary key,
-    rental_price varchar(50),
-    type varchar(500) not null,
-    brand varchar(500) not null,
-    quantity varchar(50),
-    rental_id int not null,
-    foreign key (rental_id) references rental(id) on delete set null
+            id SERIAL PRIMARY KEY,
+            rental_price VARCHAR(50),
+            type         VARCHAR(500) NOT NULL,
+            brand        VARCHAR(500) NOT NULL,
+            quantity     VARCHAR(50),
+            rental_id    INT NOT NULL,
+            FOREIGN KEY (rental_id) REFERENCES rental(id) ON DELETE SET NULL
 );
 
+
 CREATE TABLE lesson(
-    id serial primary key,
-    instructor_id int, -- not null??
-    foreign key(instructor_id) references instructor(id)
+            id SERIAL PRIMARY KEY,
+            instructor_id INT, -- not null??
+            FOREIGN KEY(instructor_id) REFERENCES instructor(id)
 );
 
 CREATE TABLE price_list(
-    price_id 
+            price_id
 );
 
 CREATE TABLE instrument_instructor(
-
+    
 );
 
 CREATE TABLE ensemble(
-
+    
 );
 
 CREATE TABLE group_lesson(
-
+    
 );
 
 CREATE TABLE individual_lesson(
-
+    
 );
 
 CREATE TABLE time_slot(
-
+    
 );

509bfd5e85c74fc684bd993acb28ba7d29b793c7
emyemy89 <40666117+emyemy89@users.noreply.github.com>
2024-11-17 11:11:13 +0100

    Update creation.sql

--- logical_and_physical_model/creation.sql
+++ logical_and_physical_model/creation.sql
@@ -35,40 +35,69 @@ CREATE TABLE contact_phone(
 );
 
 CREATE TABLE instructor(
-
+    id serial primary key,
+    employment id varchar(500) not null,
+    person_id int not null,
+    foreign key(person_id) references person(id) --delete cascade?
 );
 
 CREATE TABLE phone(
-
+    phone varchar(50) not null,
+    person_id int not null,
+    foreign key(person_id) references person(id)  --delete cascade?
 );
 
 CREATE TABLE sibling(
-
+    sibling_id varchar(50) not null,
+    student_id int not null,
+    foreign key (student_id) references student(id),
+    primary key(sibling_id,student_id)
 );
 
 CREATE TABLE student_lesson(
-
+    student_id int not null,
+    lesson_id int not null,
+    foreign key(student_id) references student(id),
+    foreign key(lesson_id) references lesson(id),
+    primary key(student_id, lesson_id)
 );
 
 CREATE TABLE instructor_availability(
-
+    availability_date date not null,
+    start_time time not null,
+    end_time time not null,
+    instructor_id int not null,
+    foreign key (instructor_id) references instructor(id),
+    primary key (availability_date, start_time, end_time, instructor_id)
 );
 
 CREATE TABLE rental (
-
+    id serial primary key,
+    start_date date not null,
+    end_date date not null,
+    student_id int not null,
+    foreign key(student_id) references student(id)
 );
 
 
 CREATE TABLE instrument(
-
+    id serial primary key,
+    rental_price varchar(50),
+    type varchar(500) not null,
+    brand varchar(500) not null,
+    quantity varchar(50),
+    rental_id int not null,
+    foreign key (rental_id) references rental(id) on delete set null
 );
 
 CREATE TABLE lesson(
-
+    id serial primary key,
+    instructor_id int, -- not null??
+    foreign key(instructor_id) references instructor(id)
 );
 
 CREATE TABLE price_list(
-
+    price_id 
 );
 
 CREATE TABLE instrument_instructor(

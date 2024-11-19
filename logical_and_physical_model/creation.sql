b35f406f791f2d5782e06dd14170a5c69803f46f
anmatar <ahmadmatar8@gmail.com>
2024-11-19 18:01:19 +0100

    update database
    add attributes constrain and change datatypes

--- logical_and_physical_model/creation.sql
+++ logical_and_physical_model/creation.sql
@@ -1,5 +1,5 @@
 CREATE TABLE person(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        person_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         first_name   VARCHAR(500) NOT NULL,
         last_name    VARCHAR(500) NOT NULL,
         personal_num VARCHAR(50) UNIQUE NOT NULL,
@@ -10,17 +10,17 @@ CREATE TABLE person(
 );
 
 CREATE TABLE student(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        student_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         level_skills      VARCHAR(50) NOT NULL,
-        contact_person_id INT NOT NULL,
+        contact_person_id INT,
         person_id         INT NOT NULL,
         FOREIGN KEY (contact_person_id) REFERENCES contact_person(contact_person_id),
-        FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+        FOREIGN KEY (person_id) REFERENCES person(person_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE contact_person(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        contact_person_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         first_name   VARCHAR(500),
         last_name    VARCHAR(500),
         personal_num VARCHAR(50) UNIQUE NOT NULL
@@ -29,36 +29,36 @@ CREATE TABLE contact_person(
 CREATE TABLE contact_phone(
         phone_nb          VARCHAR(50) NOT NULL,
         contact_person_id INT NOT NULL,
-        FOREIGN KEY (contact_person_id) REFERENCES contact_person(id) ON DELETE CASCADE,
+        FOREIGN KEY (contact_person_id) REFERENCES contact_person(contact_person_id) ON DELETE CASCADE,
         PRIMARY KEY(phone_nb, contact_person_id)
 );
 
 CREATE TABLE instructor(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        instructor_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         employment_id VARCHAR(500) UNIQUE NOT NULL,
         person_id     INT NOT NULL,
-        FOREIGN KEY(person_id) REFERENCES person(id) ON DELETE CASCADE
+        FOREIGN KEY(person_id) REFERENCES person(person_id) ON DELETE CASCADE
 );
 
 CREATE TABLE phone(
         phone_num VARCHAR(50) NOT NULL,
         person_id INT NOT NULL,
-        FOREIGN KEY(person_id) REFERENCES person(id) ON DELETE CASCADE,
+        FOREIGN KEY(person_id) REFERENCES person(person_id) ON DELETE CASCADE,
         PRIMARY KEY(phone_num, person_id)
 );
-
+---- need to be changed ----
 CREATE TABLE sibling(
-        id INT GENERATED ALWAYS AS IDENTITY,
+        sibling_student_id INT GENERATED ALWAYS AS IDENTITY,
         student_id INT NOT NULL,
-        FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE,
-        PRIMARY KEY(id,student_id)
+        FOREIGN KEY (student_id) REFERENCES student(student_id) ON DELETE CASCADE,
+        PRIMARY KEY(sibling_student_id,student_id)
 );
 
 CREATE TABLE student_lesson(
         student_id INT NOT NULL,
         lesson_id  INT NOT NULL,
-        FOREIGN KEY(student_id) REFERENCES student(id) ON DELETE CASCADE,
-        FOREIGN KEY(lesson_id) REFERENCES lesson(id) ON DELETE CASCADE,
+        FOREIGN KEY(student_id) REFERENCES student(student_id) ON DELETE CASCADE,
+        FOREIGN KEY(lesson_id) REFERENCES lesson(lesson_id) ON DELETE CASCADE,
         PRIMARY KEY(student_id, lesson_id)
 );
 
@@ -67,28 +67,30 @@ CREATE TABLE instructor_availability(
         start_time        TIME NOT NULL,
         end_time          TIME NOT NULL,
         instructor_id     INT NOT NULL,
-        FOREIGN KEY (instructor_id) REFERENCES instructor(id) ON DELETE CASCADE,
-        PRIMARY KEY (availability_date, start_time, end_time, instructor_id)
+        FOREIGN KEY (instructor_id) REFERENCES instructor(instructor_id) ON DELETE CASCADE,
+        PRIMARY KEY (availability_date, start_time, end_time, instructor_id),
+        CHECK(start_time < end_time)
 );
 
 -- WE NEED TO ENSURE THAT STARTING_DATE IS < THAN ENDING_DATE
 CREATE TABLE rental(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        rental_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         starting_date DATE NOT NULL,
         ending_date   DATE NOT NULL,
         student_id INT NOT NULL,
-        FOREIGN KEY(student_id) REFERENCES student(id) ON DELETE CASCADE
+        FOREIGN KEY(student_id) REFERENCES student(student_id) ON DELETE CASCADE,
+       -- CHECK(starting_date < ending_date AND ending_date <= starting_date + INTERVALL '12 months')
 );
 
 
 
 --should rental_price be not null??
 CREATE TABLE instrument(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        instrument_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         rental_price VARCHAR(50),
         type_of_instrument  VARCHAR(500) NOT NULL,
         brand        VARCHAR(500) NOT NULL,
-        quantity     VARCHAR(50)
+        quantity     INT CHECK(quantity > 0)
 );
 
 
@@ -96,8 +98,8 @@ CREATE TABLE instrument(
 CREATE TABLE rental_instrument(
         rental_id INT NOT NULL,
         instrument_id INT NOT NULL,
-        FOREIGN KEY (rental_id) REFERENCES rental(id) ON DELETE CASCADE,
-        FOREIGN KEY (instrument_id) REFERENCES instrument(id) ON DELETE CASCADE,
+        FOREIGN KEY (rental_id) REFERENCES rental(rental_id) ON DELETE CASCADE,
+        FOREIGN KEY (instrument_id) REFERENCES instrument(instrument_id) ON DELETE CASCADE,
         PRIMARY KEY(rental_id, instrument_id)
 );
 
@@ -106,34 +108,34 @@ CREATE TABLE rental_instrument(
 
 
 CREATE TABLE lesson(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        lesson_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         instructor_id INT NOT NULL,                                 
         price_list_id INT NOT NULL,
-        FOREIGN KEY(instructor_id) REFERENCES instructor(id),
-        FOREIGN KEY(price_list_id) REFERENCES price_list(id)
+        FOREIGN KEY(instructor_id) REFERENCES instructor(instructor_id),
+        FOREIGN KEY(price_list_id) REFERENCES price_list(price_list_id)
 );
 
 
 
 CREATE TABLE price_list(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        price VARCHAR(50),       --NOT NULL??
+        price_list_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        price VARCHAR(50) NOT NULL,       --NOT NULL?? Yes it should be not null,
         lesson_type_id INT NOT NULL,
-        skill_level_id INT NOT NULL,
-        FOREIGN KEY (lesson_type_id) REFERENCES lesson_type_lookup(id),
-        FOREIGN KEY (skill_level_id) REFERENCES skill_level_lookup(id)
+        skill_level_id INT NOT NULL,  --- 
+        FOREIGN KEY (lesson_type_id) REFERENCES lesson_type_lookup(lesson_type_lookup_id),
+        FOREIGN KEY (skill_level_id) REFERENCES skill_level_lookup(skill_level_lookup_id)
 );
 
 
 --THIS enum CORRECT?
 CREATE TABLE lesson_type_lookup(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        lesson_type_lookup_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         lesson_type VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE skill_level_lookup(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        skill_level_lookup_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         skill_level VARCHAR(50) NOT NULL
 );
 
@@ -143,18 +145,20 @@ CREATE TABLE skill_level_lookup(
 CREATE TABLE instrument_instructor(
     instrument_instructor VARCHAR(500) NOT NULL,
     instructor_id INT NOT NULL,
-    FOREIGN KEY(instructor_id) REFERENCES instructor(id) ON DELETE CASCADE,
+    FOREIGN KEY(instructor_id) REFERENCES instructor(instructor_id) ON DELETE CASCADE,
     PRIMARY KEY(instrument_instructor, instructor_id)
 );
 
 CREATE TABLE ensemble(
     lesson_id INT NOT NULL,
     genre VARCHAR(500) NOT NULL,
-    min_num_of_students VARCHAR(50) NOT NULL,
-    max_num_of_students VARCHAR(50) NOT NULL,
-    student_count VARCHAR(50),
-    FOREIGN KEY (lesson_id) REFERENCES lesson(id) ON DELETE CASCADE,
-    PRIMARY KEY(lesson_id)
+    min_num_of_students INT NOT NULL, -- why we do not store them as INT ??
+    max_num_of_students INT NOT NULL, -- why we do not store them as INT ??
+    student_count INT NOT NULL DEFAULT 0, -- why we do not store them as INT ??
+    FOREIGN KEY (lesson_id) REFERENCES lesson(lesson_id) ON DELETE CASCADE,
+    PRIMARY KEY(lesson_id),
+    CHECK (min_num_of_students <= max_num_of_students),
+    CHECK (student_count <= max_num_of_students)
 );
 
 
@@ -162,13 +166,15 @@ CREATE TABLE ensemble(
 CREATE TABLE group_lesson(
     lesson_id INT NOT NULL,
     skill_level_id INT NOT NULL,
-    min_num_of_students VARCHAR(50) NOT NULL,
-    max_num_of_students VARCHAR(50) NOT NULL,
-    student_count VARCHAR(50),
+    min_num_of_students INT NOT NULL, -- why we do not store them as INT ??
+    max_num_of_students INT NOT NULL,   -- why we do not store them as INT ??
+    student_count INT NOT NULL DEFAULT 0, -- why we do not store them as INT ??
     instrument_used VARCHAR(500),
     FOREIGN KEY (lesson_id) REFERENCES lesson(id) ON DELETE CASCADE,
-    FOREIGN KEY (skill_level_id) REFERENCES skill_level_lookup(id) ON DELETE CASCADE,
-    PRIMARY KEY(lesson_id)
+    FOREIGN KEY (skill_level_id) REFERENCES skill_level_lookup(skill_level_lookup_id) ON DELETE CASCADE,
+    PRIMARY KEY(lesson_id),
+    CHECK (min_num_of_students <= max_num_of_students),
+    CHECK (student_count<= max_num_of_students)
 );
 
 
@@ -177,7 +183,7 @@ CREATE TABLE individual_lesson(
     lesson_id INT NOT NULL,
     skill_level_id INT NOT NULL,
     FOREIGN KEY (lesson_id) REFERENCES lesson(id) ON DELETE CASCADE,
-    FOREIGN KEY (skill_level_id) REFERENCES skill_level_lookup(id) ON DELETE CASCADE,
+    FOREIGN KEY (skill_level_id) REFERENCES skill_level_lookup(skill_level_lookup_id) ON DELETE CASCADE,
     PRIMARY KEY (lesson_id)
 );
 
@@ -188,26 +194,7 @@ CREATE TABLE time_slot(
         ending_time TIME NOT NULL,
         date_slot DATE NOT NULL,
         lesson_id INT NOT NULL,
-        FOREIGN KEY (lesson_id) REFERENCES lesson(id) ON DELETE CASCADE,
+        FOREIGN KEY (lesson_id) REFERENCES lesson(lesson_id) ON DELETE CASCADE,
         PRIMARY KEY(starting_time, ending_time, date_slot,lesson_id)
 );
 
--- shoul price be of DECIAMAL type??
-CREATE TABLE student_courses_transactions(
-        date_transaction TIMESTAMP NOT NULL,
-        student_id INT NOT NULL,
-        lesson_id INT NOT NULL,
-        price VARCHAR(50) NOT NULL,
-        FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE,
-        FOREIGN KEY (lesson_id) REFERENCES lesson(id) ON DELETE CASCADE,
-        PRIMARY KEY (date_transaction, student_id, lesson_id)
-);
-
-CREATE TABLE lesson_cost_updates(
-        lesson_id INT NOT NULL,
-        update_date TIMESTAMP NOT NULL,
-        price VARCHAR(50),
-        current_price BOOLEAN,
-        FOREIGN KEY(lesson_id) REFERENCES lesson(id) ON DELETE CASCADE,
-        PRIMARY KEY (lesson_id, update_date)
-);

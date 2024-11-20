61d050a4769fcf93765cfc1729d50b8e48d4b5ff
Emanuel Paraschiv <40666117+emyemy89@users.noreply.github.com>
2024-11-20 19:21:24 +0100

    solve typos

--- logical_and_physical_model/creation.sql
+++ logical_and_physical_model/creation.sql
@@ -97,12 +97,11 @@ CREATE TABLE instrument(
 );
 
 
--- shouldn't here be 'on delete set null'??
 CREATE TABLE rental_instrument(
         rental_id INT NOT NULL,
         instrument_id INT NOT NULL,
         FOREIGN KEY (rental_id) REFERENCES rental(rental_id) ON DELETE CASCADE,
-        FOREIGN KEY (instrument_id) REFERENCES instrument(instrument_id) ON DELETE CASCADE,
+        FOREIGN KEY (instrument_id) REFERENCES instrument(instrument_id) ON DELETE SET NULL,
         PRIMARY KEY(rental_id, instrument_id)
 );
 
@@ -122,16 +121,16 @@ CREATE TABLE lesson(
 
 CREATE TABLE price_list(
         price_list_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        price VARCHAR(50) NOT NULL,       --NOT NULL?? Yes it should be not null,
-        lesson_type_id INT NOT NULL,
-        skill_level_id INT,  --- 
+        price VARCHAR(50) NOT NULL,    
+        lesson_type_lookup_id INT NOT NULL,
+        skill_level_lookup_id INT NOT NULL,  
         date_price DATE NOT NULL,
         FOREIGN KEY (lesson_type_lookup_id) REFERENCES lesson_type_lookup(lesson_type_lookup_id) ON DELETE CASCADE,
         FOREIGN KEY (skill_level_lookup_id) REFERENCES skill_level_lookup(skill_level_lookup_id) ON DELETE CASCADE
 );
 
 
---THIS enum CORRECT?
+
 CREATE TABLE lesson_type_lookup(
         lesson_type_lookup_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         lesson_type VARCHAR(50) NOT NULL
@@ -169,12 +168,12 @@ CREATE TABLE ensemble(
 
 CREATE TABLE group_lesson(
     lesson_id INT NOT NULL,
-    skill_level_id INT NOT NULL,
+    skill_level_lookup_id INT NOT NULL,
     min_num_of_students INT NOT NULL,
     max_num_of_students INT NOT NULL,   
     student_count INT NOT NULL DEFAULT 0, 
     instrument_used VARCHAR(500),
-    FOREIGN KEY (lesson_id) REFERENCES lesson(id) ON DELETE CASCADE,
+    FOREIGN KEY (lesson_id) REFERENCES lesson(lesson_id) ON DELETE CASCADE,
     FOREIGN KEY (skill_level_lookup_id) REFERENCES skill_level_lookup(skill_level_lookup_id) ON DELETE CASCADE,
     PRIMARY KEY(lesson_id),
     CHECK (min_num_of_students <= max_num_of_students),
@@ -185,8 +184,8 @@ CREATE TABLE group_lesson(
 
 CREATE TABLE individual_lesson(
     lesson_id INT NOT NULL,
-    skill_level_id INT NOT NULL,
-    FOREIGN KEY (lesson_id) REFERENCES lesson(id) ON DELETE CASCADE,
+    skill_level_lookup_id INT NOT NULL,
+    FOREIGN KEY (lesson_id) REFERENCES lesson(lesson_id) ON DELETE CASCADE,
     FOREIGN KEY (skill_level_lookup_id) REFERENCES skill_level_lookup(skill_level_lookup_id) ON DELETE CASCADE,
     PRIMARY KEY (lesson_id)
 );
@@ -199,7 +198,7 @@ CREATE TABLE time_slot(
         date_slot DATE NOT NULL,
         lesson_id INT NOT NULL,
         FOREIGN KEY (lesson_id) REFERENCES lesson(lesson_id) ON DELETE CASCADE,
-        PRIMARY KEY(starting_time, ending_time, date_slot,lesson_id)
+        PRIMARY KEY(starting_time, ending_time, date_slot,lesson_id),
         CHECK(starting_time < ending_time)
 );
 
@@ -216,7 +215,7 @@ BEGIN
         SELECT COUNT(rental_instrument.instrument_id)
         INTO instrument_count
         FROM rental_instrument
-        JOIN rental ON rental_instrument.rental_id=rental.id
+        JOIN rental ON rental_instrument.rental_id=rental.rental_id
         WHERE rental.student_id=(
                         SELECT student_id FROM rental WHERE id=NEW.rental_id
                 ) AND rental.starting_date<=(
@@ -225,7 +224,7 @@ BEGIN
                         SELECT starting_date FROM rental WHERE id=NEW.rental_id
                 );
         -- if the student tries to rent more than 2:
-        IF (instrument_count>=2) THEN RAISE EXCEPTION 'MAXIMUM NUMBER OF RENTED INSTURMENTS IS 2 PER STUDENT!' 
+        IF (instrument_count>=2) THEN RAISE EXCEPTION 'MAXIMUM NUMBER OF RENTED INSTURMENTS IS 2 PER STUDENT!'; 
         END IF;
         RETURN NEW;
         END;
@@ -235,4 +234,4 @@ BEGIN
         CREATE TRIGGER rental_instruments_limit_trigger
         BEFORE INSERT ON rental_instrument
         FOR EACH ROW
-        EXECUTE FUNCTION rental_instrument_limit();
+        EXECUTE FUNCTION rental_instruments_limit();

a564d099ae62151573dc624596c5fa24d96485bf
Emanuel Paraschiv <40666117+emyemy89@users.noreply.github.com>
2024-11-30 19:48:20 +0100

    Update historical.sql

--- OLAP_queries/historical.sql
+++ OLAP_queries/historical.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS historical; -- for debugging
+
 CREATE TABLE historical(
   lesson_id INT GENERATED ALWAYS AS IDENTITY,
   lesson_type VARCHAR(50),
@@ -16,28 +18,27 @@ INSERT INTO historical(
   instrument,
   student_first_name,
   student_last_name,
-  student_email)
+  student_email
+)
 
 SELECT 
-  lesson_type_lookup.lesson_type AS lesson_type,
+  lesson_type_lookup.lesson_type AS lesson_type, 
   price_list.price AS price,
-  ensemble.genre AS genre,
+  ensemble.genre AS genre, 
   CASE 
-        WHEN individual_lesson.lesson_id IS NOT NULL THEN individual_lesson.instrument
-        WHEN group_lesson.lesson_id IS NOT NULL THEN group_lesson.instrument_used
-        ELSE NULL
+        WHEN individual_lesson.lesson_id IS NOT NULL THEN individual_lesson.instrument  -- Get instrument for individual lessons
+        WHEN group_lesson.lesson_id IS NOT NULL THEN group_lesson.instrument_used  -- Get instrument for group lessons
+        ELSE NULL  --no instrument for ensemble
     END AS instrument,
   person.first_name AS student_first_name,
   person.last_name AS student_last_name,
   person.email AS student_email
 FROM lesson
-  JOIN student_lesson ON lesson.lesson_id=student_lesson.lesson_id -- join lesson -> person
-  JOIN student ON student_lesson.student_id= student.student_id
-  JOIN person ON student.person_id=person.person_id
-
-  JOIN price_list ON lesson.price_list_id= price_list.price_list_id -- get price
-
-  LEFT JOIN individual_lesson ON lesson.lesson_id= individual_lesson.lesson_id -- get lesson types
-  LEFT JOIN group_lesson ON lesson.lesson_id= group_lesson.lesson_id
-  LEFT JOIN ensemble ON lesson.lesson_id= ensemble.lesson_id
-  
+  JOIN student_lesson ON lesson.lesson_id = student_lesson.lesson_id  -- join lesson -> person
+  JOIN student ON student_lesson.student_id = student.student_id 
+  JOIN person ON student.person_id = person.person_id  
+  JOIN price_list ON lesson.price_list_id = price_list.price_list_id  -- join lesson -> lesson price
+  LEFT JOIN individual_lesson ON lesson.lesson_id = individual_lesson.lesson_id  -- get lesson types
+  LEFT JOIN group_lesson ON lesson.lesson_id = group_lesson.lesson_id  
+  LEFT JOIN ensemble ON lesson.lesson_id = ensemble.lesson_id  
+  LEFT JOIN lesson_type_lookup ON price_list.lesson_type_lookup_id = lesson_type_lookup.lesson_type_lookup_id;  --for lesson type

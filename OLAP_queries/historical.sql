54745eb82c29a350ed6fd8dacd29615eb1bc6e65
Emanuel Paraschiv <40666117+emyemy89@users.noreply.github.com>
2024-11-30 19:22:59 +0100

    Create historical.sql

---
+++ OLAP_queries/historical.sql
@@ -0,0 +1,43 @@
+CREATE TABLE historical(
+  lesson_id INT GENERATED ALWAYS AS IDENTITY,
+  lesson_type VARCHAR(50),
+  price VARCHAR(50),
+  genre VARCHAR(500),
+  instrument VARCHAR(500),
+  student_first_name VARCHAR(500),
+  student_last_name VARCHAR(500),
+  student_email VARCHAR(500)
+);
+
+INSERT INTO historical(
+  lesson_type,
+  price,
+  genre,
+  instrument,
+  student_first_name,
+  student_last_name,
+  student_email)
+
+SELECT 
+  lesson_type_lookup.lesson_type AS lesson_type,
+  price_list.price AS price,
+  ensemble.genre AS genre,
+  CASE 
+        WHEN individual_lesson.lesson_id IS NOT NULL THEN individual_lesson.instrument
+        WHEN group_lesson.lesson_id IS NOT NULL THEN group_lesson.instrument_used
+        ELSE NULL
+    END AS instrument,
+  person.first_name AS student_first_name,
+  person.last_name AS student_last_name,
+  person.email AS student_email
+FROM lesson
+  JOIN student_lesson ON lesson.lesson_id=student_lesson.lesson_id -- join lesson -> person
+  JOIN student ON student_lesson.student_id= student.student_id
+  JOIN person ON student.person_id=person.person_id
+
+  JOIN price_list ON lesson.price_list_id= price_list.price_list_id -- get price
+
+  LEFT JOIN individual_lesson ON lesson.lesson_id= individual_lesson.lesson_id -- get lesson types
+  LEFT JOIN group_lesson ON lesson.lesson_id= group_lesson.lesson_id
+  LEFT JOIN ensemble ON lesson.lesson_id= ensemble.lesson_id
+  
